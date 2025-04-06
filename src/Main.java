4925f5a40df2977d6359fa76e13a04959b90b362
jarvychavez <jav.chavez94@gmail.com>
2025-04-06 15:32:47 -0600

    pelicula de matrix

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!");
+        System.out.println("Bienvenido(a) a Screen Match");
+        System.out.println("Película: Matrix");
+
+       int fechaDeLanzamiento = 1999;
+       boolean incluidoEnElPlan = true;
+       double notaDeLaPelicula = 8.2;
+
+       double media = (8.2 + 6.0 + 9.0) /3;
+        System.out.println(media);
+
     }
 }
 
