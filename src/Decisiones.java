00845099c6e06895c4d84da49cb45b41f32c45e2
jarvychavez <jav.chavez94@gmail.com>
2025-04-07 15:34:53 -0600

    clase de deciciones

---
+++ src/Decisiones.java
@@ -0,0 +1,14 @@
+public class Decisiones {
+
+    public static void main(String[] args) {
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+
+        if (fechaDeLanzamiento >= 2022){
+            System.out.println("Péliculas más populares");
+        }else{
+            System.out.println("Péliculas Retro que aún vale la pena ver");
+        }
+    }
+}
