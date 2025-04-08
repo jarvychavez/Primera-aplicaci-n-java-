fa80d116e9392b62090ecff8b950e093939c4a40
jarvychavez <jav.chavez94@gmail.com>
2025-04-07 21:24:57 -0600

    icluir el plan

--- src/Decisiones.java
+++ src/Decisiones.java
@@ -4,11 +4,19 @@ public class Decisiones {
         int fechaDeLanzamiento = 1999;
         boolean incluidoEnElPlan = true;
         double notaDeLaPelicula = 8.2;
-
+        String tipoPlan = "plus";
+// código que prueba los operadores relacionales
         if (fechaDeLanzamiento >= 2022){
             System.out.println("Péliculas más populares");
         }else{
             System.out.println("Péliculas Retro que aún vale la pena ver");
         }
+
+        if (incluidoEnElPlan && tipoPlan.equals("plus") ){
+            System.out.println("Disfrute de su pélicula");
+        }else{
+            System.out.println("Pélicula no disponible para su plan actual");
+        }
+
     }
 }
